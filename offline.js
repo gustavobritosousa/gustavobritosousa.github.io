2486bbaab80f1f6be1bb48e19a3cc07b2a4dcb79
gustavobritosousa <44906742+gustavobritosousa@users.noreply.github.com>
2018-11-09 16:27:04 -0200

    Jogo

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1541787867,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/plataforma-sheet0.png",
+		"images/bloco1-sheet0.png",
+		"images/maça-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/particles.png",
+		"images/fundomenu.png",
+		"images/btnplay-sheet0.png",
+		"images/btnclose-sheet0.png",
+		"images/btnplay2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/paredeinv-sheet0.png",
+		"images/btn1-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
